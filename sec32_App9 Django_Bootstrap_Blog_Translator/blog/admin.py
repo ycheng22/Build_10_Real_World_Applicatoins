455e6289c8ecde0cd9e3a299a1a7feab576899bf
ycheng <36308352+ycheng22@users.noreply.github.com>
2021-10-13 16:59:59 -0400

    Add files via upload

---
+++ sec32_App9 Django_Bootstrap_Blog_Translator/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'author') # the name are from models.Post
+
+# Register your models here.
+admin.site.register(Post, PostAdmin)
